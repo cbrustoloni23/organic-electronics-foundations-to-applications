c70b284ea05dd2e40b29f8296ca7b7f515b572e6
Cbrustoloni23 <cbrustol@umich.edu>
2026-07-24 15:58:43 -0400

    fix login redirect to home

--- login-form.php
+++ login-form.php
@@ -1,8 +1,6 @@
 <?php
-if (! empty($_POST["login-btn"])) {
-    require_once './Model/Member.php';
-    $member = new Member();
-    $loginResult = $member->loginMember();
+if (!isset($loginResult)) {
+    $loginResult = "";
 }
 ?>
 		<div class="sign-up-container">
