21781494f174f27932fa9967560a95439dc2dad5
Cbrustoloni23 <cbrustol@umich.edu>
2026-07-24 18:31:18 -0400

    Add files from local computer

--- Model/Member.php
+++ Model/Member.php
@@ -8,7 +8,8 @@ class Member
 
     function __construct()
     {
-        require_once __DIR__ . './../lib/DataSource.php';
+        require_once __DIR__ . '/../lib/DataSource.php';
+
         $this->ds = new DataSource();
     }
 
