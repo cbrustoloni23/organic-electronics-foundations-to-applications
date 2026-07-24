49c460eb1055683fec907d376bb838a73dd17909
Cbrustoloni23 <cbrustol@umich.edu>
2026-07-24 16:33:34 -0400

    Add files from local computer

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
-<?php 
-use Phppot\Member;
-
+<?php
 session_start();
+
+$loginResult = "";
+
+if (!empty($_POST["login-btn"])) {
+    require_once __DIR__ . "/Model/Member.php";
+
+    $member = new \Phppot\Member();
+    $loginResult = $member->loginMember();
+}
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
